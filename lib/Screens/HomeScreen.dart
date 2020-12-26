578890a98953faa6d7f98e7b0ff773d66b5e4042
Saagar Parikh <saagardp@gmail.com>
2020-12-27 04:14:52 +0530

    HomePageUiChanges2
    Linear gradient done.

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -21,38 +21,43 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final eventsDataProvider = Provider.of<EventsDataProvider>(context);
     final List<HomeScreenModel> eventsList = eventsDataProvider.eventsList;
+    final Shadow dropShadow2 = new Shadow(
+      blurRadius: 5,
+      color: Color(0x55000000),
+      offset: Offset(4, 4),
+    );
 
     final List bottomScrollingList = [
       {
         'name': 'Events',
-        'color': Colors.purpleAccent,
+        'color': [Color(0xff9a68f8), Color(0xff8a48fd), Color(0xff7320fe)],
         'route': 'None',
       },
       {
         'name': 'Sponsors',
-        'color': Colors.pink,
+        'color': [Color(0xffff6ed1), Color(0xffff21e0), Color(0xffff27e0)],
         'route': 'None',
       },
       {
         'name': 'CA',
-        'color': Colors.blue,
+        'color': [Color(0xff5ea8fe), Color(0xff3e93fe), Color(0xff347def)],
         'route': 'None',
       },
       {
         'name': 'Our Team',
-        'color': Colors.lightBlue,
+        'color': [Color(0xff27d1e6), Color(0xff1dc9de), Color(0xff12b7cc)],
         'route': 'None',
       },
       {
         'name': 'Contact Us',
-        'color': Colors.pink,
+        'color': [Color(0xff3ee4ab), Color(0xff07e19d), Color(0xff03beab)],
         'route': 'None',
       },
     ];
 
     //responsive media query
     final data = MediaQuery.of(context);
-    print(data.size);
+    // print(data.size);
 
     double _screenHeight;
     double _screenWidth;
@@ -69,8 +74,6 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Color(0xff1e2025),
-
-      /// all this has to be changed to make the look better ,
       appBar: AppBar(
         backgroundColor: Color(0xff1e2025),
       ),
@@ -78,38 +81,15 @@ class HomeScreen extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              // An exammple showing the shadow effect thats there in the design
-
-              //           Text(
-              // 'Hello, world!',
-              // style: TextStyle(
-              //   shadows: <Shadow>[
-              //     Shadow(
-              //       offset: Offset(10.0, 10.0),
-              //       blurRadius: 3.0,
-              //       color: Color.fromARGB(255, 0, 0, 0),
-              //     ),
-              //     Shadow(
-              //       offset: Offset(10.0, 10.0),
-              //       blurRadius: 8.0,
-              //       color: Color.fromARGB(125, 0, 0, 255),
-              //     ),
-              //   ],
-              // ),
-              // ),
-
               Padding(
                 padding:
                     EdgeInsets.fromLTRB(_screenWidth * 0.05, 0.0, 0.0, 0.0),
-                // padding: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    /// Set some gradient to match the color of text with the poster ///
                     'Blithchron',
                     style: TextStyle(
                       fontSize: _screenHeight * 0.075,
-                      // fontSize: 63, // TODO: Change size according to screensize
                       fontWeight: FontWeight.w900,
                       foreground: Paint()..shader = linearGradientTitle,
                     ),
@@ -119,33 +99,23 @@ class HomeScreen extends StatelessWidget {
               ),
               Center(
                 child: Text(
-                  /// Set some gradient to match the color of text with the poster ///
                   'A CONFLUENCE OF ECSTACIES',
                   style: TextStyle(
                     fontSize: _screenHeight * 0.02754,
-                    // fontSize: 23, // TODO: Change size according to screensize
                     fontWeight: FontWeight.w900,
                     foreground: Paint()..shader = linearGradientTitle,
                   ),
                 ),
               ),
               SizedBox(
-                height: _screenHeight * 0.0359,
-                // height: 30,
-
-                /// make this responsive for all screen sizes
+                height: _screenHeight * 0.05,
               ),
               Padding(
                 padding: EdgeInsets.fromLTRB(
-                    _screenWidth * 0.05, 0.0, _screenWidth * 0.05, 0.0),
-                // TODO: width needs to be changed according to screen size
-                // padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                    _screenWidth * 0.032, 0.0, _screenWidth * 0.032, 0.0),
                 child: Center(
                   child: Container(
-                    /// add all the shadow as there in the design
-                    /// first block
                     decoration: BoxDecoration(
-                      // border: Border.all(color: Colors.blue, width: 2.0),
                       color: Color(0xff26292e),
                       borderRadius: BorderRadius.all(
                         Radius.circular(20.0),
@@ -162,19 +132,19 @@ class HomeScreen extends StatelessWidget {
                       children: [
                         Padding(
                           padding: EdgeInsets.all(_screenHeight * 0.0095),
-                          // padding: const EdgeInsets.all(8.0),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: [
                               Padding(
                                 padding: EdgeInsets.all(_screenWidth * 0.03),
-                                // padding: const EdgeInsets.all(12.0),
                                 child: Text(
                                   'Upcoming Events',
                                   style: TextStyle(
+                                      shadows: [
+                                        dropShadow2,
+                                      ],
                                       color: Colors.white,
-                                      fontSize: _screenHeight * 0.0215,
-                                      // fontSize: 18,
+                                      fontSize: _screenHeight * 0.0255,
                                       fontWeight: FontWeight.w900),
                                 ),
                               ),
@@ -182,10 +152,7 @@ class HomeScreen extends StatelessWidget {
                           ),
                         ),
                         Container(
-                          /// Height will be fixed, but a different value than 440
-                          /// according to height and width of list item
-                          // height: 440,
-                          height: _screenHeight * 0.527,
+                          height: _screenHeight * 0.68,
                           child: ListView.builder(
                             /// @Gautam this doesn't need to be a scrolling list,
                             /// can we make it a static one? It's affecting the
@@ -195,6 +162,7 @@ class HomeScreen extends StatelessWidget {
                               HomeScreenModel currentEvent = eventsList[i];
                               return HomeScreenItem(
                                 dateDay: currentEvent.dateDay,
+                                dateSuffix: currentEvent.dateSuffix,
                                 dateMonth: currentEvent.dateMonth,
                                 eventName: currentEvent.eventName,
                                 eventDescription: currentEvent.eventDescription,
@@ -210,13 +178,11 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                // height: 20,
                 height: _screenHeight * 0.02395,
               ),
               Padding(
                 padding: EdgeInsets.fromLTRB(
-                    _screenWidth * 0.05, 0.0, _screenWidth * 0.05, 0.0),
-                // padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                    _screenWidth * 0.032, 0.0, _screenWidth * 0.032, 0.0),
                 child: Center(
                   child: Container(
                     decoration: BoxDecoration(
@@ -248,8 +214,11 @@ class HomeScreen extends StatelessWidget {
                                   child: Text(
                                     'Discover',
                                     style: TextStyle(
+                                        shadows: [
+                                          dropShadow2,
+                                        ],
                                         color: Colors.white,
-                                        fontSize: _screenHeight * 0.0215,
+                                        fontSize: _screenHeight * 0.0255,
                                         // fontSize: 18,
                                         fontWeight: FontWeight.w900),
                                   ),
@@ -268,7 +237,10 @@ class HomeScreen extends StatelessWidget {
                                         return InkWell(
                                           onTap: () {},
                                           child: Padding(
-                                            padding: EdgeInsets.all(
+                                            padding: EdgeInsets.fromLTRB(
+                                                _screenWidth * 0.005,
+                                                _screenWidth * 0.005,
+                                                _screenWidth * 0.025,
                                                 _screenWidth * 0.005),
                                             // padding: const EdgeInsets.all(2.0),
                                             child: Container(
@@ -281,47 +253,53 @@ class HomeScreen extends StatelessWidget {
                                                 ),
                                                 boxShadow: <BoxShadow>[
                                                   new BoxShadow(
-                                                    color: Color(0x77000000),
-                                                    blurRadius: 5.0,
-                                                    offset:
-                                                        new Offset(5.0, 5.0),
+                                                    color: Color(0x55000000),
+                                                    blurRadius: 5,
+                                                    offset: new Offset(4, 4),
                                                   ),
                                                 ],
                                               ),
-                                              child: Card(
-                                                  color: bottomScrollingList[i]
-                                                      ['color'],
-                                                  child: Center(
-                                                    child: Column(
-                                                      children: [
-                                                        SizedBox(
-                                                          height:
-                                                              _screenHeight *
-                                                                  0.01198,
-                                                          // height: 10,
-                                                        ),
-                                                        Text(
-                                                          bottomScrollingList[i]
-                                                              ['name'],
-                                                          style: TextStyle(
-                                                            color: Colors.white,
-                                                            fontWeight:
-                                                                FontWeight.bold,
-                                                          ),
-                                                        ),
-                                                        SizedBox(
-                                                          height:
-                                                              _screenHeight *
-                                                                  0.01198,
-                                                          // height: 10,
-                                                        ),
-                                                        Icon(
-                                                          Icons
-                                                              .arrow_forward_sharp,
-                                                        )
-                                                      ],
+                                              child: Container(
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.all(
+                                                      Radius.circular(10.0),
                                                     ),
-                                                  )),
+                                                    gradient: LinearGradient(
+                                                      colors:
+                                                          bottomScrollingList[i]
+                                                              ['color'],
+                                                    )),
+                                                child: Center(
+                                                  child: Column(
+                                                    children: [
+                                                      SizedBox(
+                                                        height: _screenHeight *
+                                                            0.02198,
+                                                        // height: 10,
+                                                      ),
+                                                      Text(
+                                                        bottomScrollingList[i]
+                                                            ['name'],
+                                                        style: TextStyle(
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                        ),
+                                                      ),
+                                                      SizedBox(
+                                                        height: _screenHeight *
+                                                            0.01198,
+                                                        // height: 10,
+                                                      ),
+                                                      Icon(
+                                                        Icons
+                                                            .arrow_forward_sharp,
+                                                      )
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
                                             ),
                                           ),
                                         );
